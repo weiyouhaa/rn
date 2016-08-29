d7e1d2edaf4bbc538b95117e491e38630e7885ab
weiyouhaa <weiyoushuai1991@gmail.com>
2016-08-29 23:42:29 +0800

    ok

---
+++ AwesomeProject/style/styles.js
@@ -0,0 +1,13 @@
+'use strict';
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+
+var styles1 = StyleSheet.create({
+    text:{
+    	fontSize:66,
+    	borderWidth:2,
+    	borderColor:"#203A3D",
+    	margin:2,
+    	
+    }
+});
+module.exports = styles1;
